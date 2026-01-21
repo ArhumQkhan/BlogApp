5b62a40fe09742364654d5deaa2edd1678bc0475
M. Arhum Qayyum Khan <khanarhumqayyum@gmail.com>
2026-01-21 22:48:07 +0500

    test commit

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -15,7 +15,6 @@ from datetime import timedelta
 import environ
 import os
 
-#just a test comment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
